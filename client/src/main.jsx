50cb0771ade83e73bbea76552cafafa1eacc5d13
edrg2 <hibluesky1125@gmail.com>
2025-06-20 20:48:20 +0800

    mobile nav v1

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,7 @@ import { store } from "./app/store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    {/* 啟用 Redux */}
     <Provider store={store}>
       <App />
     </Provider>
